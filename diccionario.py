72c58320b510a4f710ca0fb7fa58d409752ea6d1
Alex Marquez <alex.marquez.15@outlook.com>
2020-09-06 18:15:00 -0300

    Solo declaracion de variables

---
+++ diccionario.py
@@ -0,0 +1,6 @@
+alumnos=('Alex','Carla','Jose','Lucia')
+#Invente los nombres
+fisicaNotas=[5.5,5.3,6.4,5.6]
+programacionNotas=[5.5,7.0,5.4,6.4]
+#Todos con buenas notas!!
+diccionario={'Nombre':alumnos,'Programacion':programacionNotas,'Fisica':fisicaNotas}
---
+++ operaciones.py
@@ -0,0 +1,4 @@
+x=10
+y=2.5
+z=6
+print (f'Para x={x}, y={y}, z={z}')
