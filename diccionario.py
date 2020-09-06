82194b1b0ef1a01fe8f530083432dd40176ad57d
Alex Marquez <alex.marquez.15@outlook.com>
2020-09-06 18:27:29 -0300

    Programa funcionando

--- diccionario.py
+++ diccionario.py
@@ -4,3 +4,7 @@ fisicaNotas=[5.5,5.3,6.4,5.6]
 programacionNotas=[5.5,7.0,5.4,6.4]
 #Todos con buenas notas!!
 diccionario={'Nombre':alumnos,'Programacion':programacionNotas,'Fisica':fisicaNotas}
+print ('Nombres: ', diccionario['Nombre'])
+print ('Notas Programacion: ', diccionario['Programacion'])
+print ('Notas Fisica: ', diccionario['Fisica'])
+
--- operaciones.py
+++ operaciones.py
@@ -2,3 +2,9 @@ x=10
 y=2.5
 z=6
 print (f'Para x={x}, y={y}, z={z}')
+print (f'{x} más {y} es {x+y}')
+print (f'{x} menos {y} es {x-y}')
+print (f'{x} multiplicado por {y} es {x*y}')
+print (f'{x} dividido por {y} es {x/y}')
+print (f'El resto de {x} dividido por {z} es {x%z}')
+
